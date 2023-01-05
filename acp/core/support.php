5548faf8cf39c328b504ca922e4cbde1b04a500e
Patrick <pk@konstand.de>
2023-01-05 16:08:07 +0100

    add links to documentation

--- acp/core/support.php
+++ acp/core/support.php
@@ -35,13 +35,28 @@ if($show_open_source_docs == true) {
     echo '<div class="card">';
     echo '<div class="card-body">';
 
-    /* get the support contents from swiftyedit.net */
+    /**
+     * @var array $docs_list from .../docs-list.php
+     */
+    $doc_list_file = '../lib/lang/'.$languagePack.'/docs-list.php';
+    if(is_file($doc_list_file)) {
+        include $doc_list_file;
+        echo '<div class="list-group">';
+        foreach($docs_list as $k => $v) {
+            echo '<a href="'.$v['url'].'" title="'.$v['url'].'" 
+            class="list-group-item list-group-item-action"
+            target="_blank">'.$v['title'].' <i class="bi bi-arrow-right-short"></i></a>';
+        }
+        echo '</div>';
+        echo '<hr>';
+    }
+
 
     echo $lang['msg_community_edition'];
 
     echo '<ul>';
-    echo '<li>SwiftyEdit.com</li>';
-    echo '<li>GitHub.com</li>';
+    echo '<li><a href="https://SwiftyEdit.org" title="" target="_blank">SwiftyEdit.org</a></li>';
+    echo '<li><a href="https://github.com/SwiftyEdit/" title="" target="_blank">GitHub.com</a></li>';
     echo '</ul>';
 
     echo '</div>';
---
+++ lib/lang/de/docs-list.php
@@ -0,0 +1,14 @@
+<?php
+
+$docs_list = [
+    ['url' => 'https://swiftyedit.org/de/dokumentation/','title' => 'Dokumentation'],
+    ['url' => 'https://swiftyedit.org/de/dokumentation/benutzerhandbuch/seiten-verwalten/','title' => 'Seiten bearbeiten'],
+    ['url' => 'https://swiftyedit.org/de/dokumentation/benutzerhandbuch/shortcodes/','title' => 'Shortcodes'],
+    ['url' => 'https://swiftyedit.org/de/dokumentation/benutzerhandbuch/snippets/','title' => 'Snippets'],
+    ['url' => 'https://swiftyedit.org/de/dokumentation/benutzerhandbuch/blog/','title' => 'Blog'],
+    ['url' => 'https://swiftyedit.org/de/dokumentation/benutzerhandbuch/shop/','title' => 'Shop'],
+    ['url' => 'https://swiftyedit.org/de/dokumentation/benutzerhandbuch/events/','title' => 'Events'],
+    ['url' => 'https://swiftyedit.org/de/dokumentation/benutzerhandbuch/uploads/','title' => 'Uploads'],
+    ['url' => 'https://swiftyedit.org/de/dokumentation/benutzerhandbuch/benutzer/','title' => 'Benutzerverwaltung'],
+    ['url' => 'https://swiftyedit.org/de/dokumentation/benutzerhandbuch/einstellungen/','title' => 'Einstellungen']
+];
