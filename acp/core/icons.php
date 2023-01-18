ddbcbcdb6e4a2c55fef1d30575dedcee8cd060a1
Patrick <pk@konstand.de>
2023-01-18 14:04:34 +0100

    reformat / sort icons

--- acp/core/icons.php
+++ acp/core/icons.php
@@ -6,164 +6,142 @@
 
 $icon = [
     "activity" => '<i class="bi bi-activity"></i>',
-    "arrow_repeat" => '<i class="bi bi-arrow-repeat"></i>',
-    "sync_alt" => '<i class="bi bi-arrow-repeat"></i>',
     "arrow_down" => '<i class="bi bi-arrow-down"></i>',
-    "arrow_up" => '<i class="bi bi-arrow-up"></i>',
     "arrow_left" => '<i class="bi bi-arrow-left"></i>',
+    "arrow_repeat" => '<i class="bi bi-arrow-repeat"></i>',
     "arrow_right" => '<i class="bi bi-arrow-right"></i>',
     "arrow_right_short" => '<i class="bi bi-arrow-right-short"></i>',
-    "three_dots" => '<i class="bi bi-three-dots"></i>'
+    "arrow_up" => '<i class="bi bi-arrow-up"></i>',
+    "at" => '<i class="bi bi-at"></i>',
+
+    "ban" => '<i class="bi bi-slash-circle"></i>',
+    "bars" => '<i class="bi bi-chart-bar"></i>',
+    "bookmark" => '<i class="bi bi-bookmark"></i>',
+
+    "calendar_check" => '<i class="bi bi-calendar-check"></i>',
+    "calendar_event" => '<i class="bi bi-calendar-event"></i>',
+    "caret_down" => '<i class="bi bi-caret-down"></i>',
+    "caret_left" => '<i class="bi bi-caret-left"></i>',
+    "caret_right" => '<i class="bi bi-caret-right"></i>',
+    "caret_right_fill" => '<i class="bi bi-caret-right-fill"></i>',
+    "caret_up" => '<i class="bi bi-caret-up"></i>',
+    "cart" => '<i class="bi bi-cart"></i>',
+    "chart_bar" => '<i class="bi bi-bar-chart"></i>',
+    "chat_square_dots" => '<i class="bi bi-chat-square-dots"></i>',
+    "check" => '<i class="bi bi-check"></i>',
+    "check_circle" => '<i class="bi bi-check-circle"></i>',
+    "check_lg" => '<i class="bi bi-check-lg"></i>',
+    "circle" => '<i class="bi bi-circle"></i>',
+    "circle_alt" => '<i class="bi bi-circle"></i>',
+    "clipboard" => '<i class="bi bi-clipboard"></i>',
+    "clipboard_list" => '<i class="bi bi-clipboard-fill"></i>',
+    "clock" => '<i class="bi bi-clock"></i>',
+    "code" => '<i class="bi bi-code"></i>',
+    "code_slash" => '<i class="bi bi-code-slash"></i>',
+    "cog" => '<i class="bi bi-gear"></i>',
+    "cogs" => '<i class="bi bi-gear"></i>',
+    "comments" => '<i class="bi bi-chat"></i>',
+    "compress" => '<i class="bi bi-file-earmark-zip"></i>',
+    "copy" => '<i class="bi bi-clipboard-plus"></i>',
+
+    "database" => '<i class="bi bi-hdd-network"></i>',
+    "diagram_3" => '<i class="bi bi-diagram-3"></i>',
+    "dot_circle" => '<i class="bi bi-circle"></i>',
+    "download" => '<i class="bi bi-download"></i>',
+
+    "edit" => '<i class="bi bi-pencil-square"></i>',
+    "envelope" => '<i class="bi bi-envelope"></i>',
+    "exclamation_triangle" => '<i class="bi bi-exclamation-triangle"></i>',
+    "eye" => '<i class="bi bi-eye"></i>',
+    "eye_slash" => '<i class="bi bi-eye-slash"></i>',
+
+    "file" => '<i class="bi bi-file-earmark"></i>',
+    "file_alt" => '<i class="bi bi-file-earmark"></i>',
+    "file_earmark_post" => '<i class="bi bi-file-earmark-post"></i>',
+    "files" => '<i class="bi bi-files"></i>',
+    "filter" => '<i class="bi bi-filter"></i>',
+    "folder" => '<i class="bi bi-folder"></i>',
+    "folder_open" => '<i class="bi bi-folder2-open"></i>',
+
+    "gear" => '<i class="bi bi-gear"></i>',
+    "github" => '<i class="bi bi-github"></i>',
+
+    "home" => '<i class="bi bi-house"></i>',
+    "house" => '<i class="bi bi-house"></i>',
+
+    "images" => '<i class="bi bi-images"></i>',
+    "inbox" => '<i class="bi bi-inbox"></i>',
+    "info_circle" => '<i class="bi bi-info-circle"></i>',
+
+    "language" => '<i class="bi bi-translate"></i>',
+    "level_up_alt" => '<i class="bi bi-arrow-90deg-up"></i>',
+    "link" => '<i class="bi bi-link"></i>',
+    "list" => '<i class="bi bi-list"></i>',
+    "lock" => '<i class="bi bi-lock"></i>',
+    "lock_open" => '<i class="bi bi-unlock"></i>',
+    "logout" => '<i class="bi bi-box-arrow-in-left"></i>',
+    "long_arrow_alt_right" => '<i class="bi bi-arrow-right"></i>',
+
+    "mailbox" => '<i class="bi bi-mailbox"></i>',
+    "map_marker_alt" => '<i class="bi bi-geo-alt"></i>',
+
+    "palette" => '<i class="bi bi-palette"></i>',
+    "palette2" => '<i class="bi bi-palette2"></i>',
+    "paper_clip" => '<i class="bi bi-paperclip"></i>',
+    "paper_plane" => '<i class="bi bi-send"></i>',
+    "paste" => '<i class="bi bi-clipboard-plus"></i>',
+    "people" => '<i class="bi bi-people"></i>',
+    "people_fill" => '<i class="bi bi-people-fill"></i>',
+    "person" => '<i class="bi bi-person"></i>',
+    "person_check" => '<i class="bi bi-person-check"></i>',
+    "person_dash" => '<i class="bi bi-person-dash"></i>',
+    "person_fill" => '<i class="bi bi-person-fill"></i>',
+    "person_plus" => '<i class="bi bi-person-plus"></i>',
+    "person_x" => '<i class="bi bi-person-x"></i>',
+    "plugin" => '<i class="bi bi-plugin"></i>',
+    "plus" => '<i class="bi bi-plus"></i>',
+
+    "question" => '<i class="bi bi-question"></i>',
+
+    "rss" => '<i class="bi bi-rss"></i>',
+
+    "save" => '<i class="bi bi-save"></i>',
+    "search" => '<i class="bi bi-search"></i>',
+    "server" => '<i class="bi bi-server"></i>',
+    "share" => '<i class="bi bi-share"></i>',
+    "shop" => '<i class="bi bi-shop"></i>',
+    "shopping_basket" => '<i class="bi bi-basket"></i>',
+    "sign_out_alt" => '<i class="bi bi-box-arrow-left"></i>',
+    "sitemap" => '<i class="bi bi-diagram-3"></i>',
+    "speedometer" => '<i class="bi bi-speedometer"></i>',
+    "star" => '<i class="bi-star-fill"></i>',
+    "star_outline" => '<i class="bi bi-star"></i>',
+    "store" => '<i class="bi bi-shop"></i>',
+    "sync_alt" => '<i class="bi bi-arrow-repeat"></i>',
+
+    "table" => '<i class="bi bi-table"></i>',
+    "tags" => '<i class="bi bi-tags"></i>',
+    "tasks" => '<i class="bi bi-list-task"></i>',
+    "three_dots" => '<i class="bi bi-three-dots"></i>',
+    "thumbs_down" => '<i class="bi bi-hand-thumbs-down"></i>',
+    "thumbs_up" => '<i class="bi bi-hand-thumbs-up"></i>',
+    "times_circle" => '<i class="bi bi-clock"></i>',
+    "toggle_off" => '<i class="bi bi-toggle-off"></i>',
+    "toggle_on" => '<i class="bi bi-toggle-on"></i>',
+    "tools" => '<i class="bi bi-tools"></i>',
+    "translate" => '<i class="bi bi-translate"></i>',
+    "trash" => '<i class="bi bi-trash"></i>',
+    "trash_alt" => '<i class="bi bi-trash"></i>',
+
+    "upload" => '<i class="bi bi-upload"></i>',
+    "user" => '<i class="bi bi-person"></i>',
+    "user_check" => '<i class="bi bi-person-check"></i>',
+    "user_friends" => '<i class="bi bi-person-hearts"></i>',
+    "user_lock" => '<i class="bi bi-person-x"></i>',
+    "user_minus" => '<i class="bi bi-person-dash"></i>',
+    "user_plus" => '<i class="bi bi-person-plus"></i>',
+    "user_slash" => '<i class="bi bi-person-dash"></i>',
+    "users" => '<i class="bi bi-people"></i>',
+    
+    "wrench" => '<i class="bi bi-wrench"></i>'
 ];
-
-
-
-$icon['cart'] = '<i class="bi bi-cart"></i>';
-$icon['calendar_event'] = '<i class="bi bi-calendar-event"></i>';
-$icon['calendar_check'] = '<i class="bi bi-calendar-check"></i>';
-$icon['chat_square_dots'] = '<i class="bi bi-chat-square-dots"></i>';
-$icon['caret_left'] = '<i class="bi bi-caret-left"></i>';
-$icon['caret_right'] = '<i class="bi bi-caret-right"></i>';
-$icon['caret_down'] = '<i class="bi bi-caret-down"></i>';
-$icon['caret_up'] = '<i class="bi bi-caret-up"></i>';
-$icon['caret_right_fill'] = '<i class="bi bi-caret-right-fill"></i>';
-$icon['check'] = '<i class="bi bi-check"></i>';
-$icon['check_lg'] = '<i class="bi bi-check-lg"></i>';
-$icon['clipboard'] = '<i class="bi bi-clipboard"></i>';
-$icon['code'] = '<i class="bi bi-code"></i>';
-$icon['code_slash'] = '<i class="bi bi-code-slash"></i>';
-
-$icon['speedometer'] = '<i class="bi bi-speedometer"></i>';
-$icon['diagram_3'] = '<i class="bi bi-diagram-3"></i>';
-$icon['sitemap'] = '<i class="bi bi-diagram-3"></i>';
-
-$icon['edit'] = '<i class="bi bi-pencil-square"></i>';
-$icon['envelope'] = '<i class="bi bi-envelope"></i>';
-
-$icon['files'] = '<i class="bi bi-files"></i>';
-$icon['file_earmark_post'] = '<i class="bi bi-file-earmark-post"></i>';
-$icon['folder'] = '<i class="bi bi-folder"></i>';
-$icon['gear'] = '<i class="bi bi-gear"></i>';
-
-$icon['house'] = '<i class="bi bi-house"></i>';
-$icon['home'] = '<i class="bi bi-house"></i>';
-
-$icon['images'] = '<i class="bi bi-images"></i>';
-$icon['inbox'] = '<i class="bi bi-inbox"></i>';
-
-$icon['logout'] = '<i class="bi bi-box-arrow-in-left"></i>';
-
-$icon['mailbox'] = '<i class="bi bi-mailbox"></i>';
-
-$icon['palette'] = '<i class="bi bi-palette"></i>';
-$icon['palette2'] = '<i class="bi bi-palette2"></i>';
-
-$icon['plugin'] = '<i class="bi bi-plugin"></i>';
-$icon['upload'] = '<i class="bi bi-upload"></i>';
-$icon['people'] = '<i class="bi bi-people"></i>';
-$icon['people_fill'] = '<i class="bi bi-people-fill"></i>';
-$icon['person'] = '<i class="bi bi-person"></i>';
-$icon['person_fill'] = '<i class="bi bi-person-fill"></i>';
-$icon['person_check'] = '<i class="bi bi-person-check"></i>';
-$icon['person_dash'] = '<i class="bi bi-person-dash"></i>';
-$icon['person_plus'] = '<i class="bi bi-person-plus"></i>';
-$icon['person_x'] = '<i class="bi bi-person-x"></i>';
-$icon['plus'] = '<i class="bi bi-plus"></i>';
-
-$icon['question'] = '<i class="bi bi-question"></i>';
-
-$icon['rss'] = '<i class="bi bi-rss"></i>';
-
-$icon['save'] = '<i class="bi bi-save"></i>';
-
-$icon['shop'] = '<i class="bi bi-shop"></i>';
-$icon['translate'] = '<i class="bi bi-translate"></i>';
-$icon['trash'] = '<i class="bi bi-trash"></i>';
-$icon['trash_alt'] = '<i class="bi bi-trash"></i>';
-
-
-$icon['at']  = '<i class="bi bi-at"></i>';
-
-$icon['ban'] = '<i class="bi bi-slash-circle"></i>';
-$icon['bars'] = '<i class="bi bi-chart-bar"></i>';
-$icon['bookmark'] = '<i class="bi bi-bookmark"></i>';
-
-$icon['calendar_check'] = '<i class="bi bi-calendar-check"></i>';
-$icon['circle_alt'] = '<i class="bi bi-circle"></i>';
-$icon['check_circle'] = '<i class="bi bi-check-circle"></i>';
-
-$icon['circle'] = '<i class="bi bi-circle"></i>';
-$icon['clipboard_list'] = '<i class="bi bi-clipboard-fill"></i>';
-$icon['comments'] = '<i class="bi bi-chat"></i>';
-$icon['compress']  = '<i class="bi bi-file-earmark-zip"></i>';
-$icon['copy']  = '<i class="bi bi-clipboard-plus"></i>';
-$icon['clock']  = '<i class="bi bi-clock"></i>';
-$icon['cogs'] = '<i class="bi bi-gear"></i>';
-$icon['cog'] = '<i class="bi bi-gear"></i>';
-
-$icon['exclamation_triangle']  = '<i class="bi bi-exclamation-triangle"></i>';
-$icon['eye']  = '<i class="bi bi-eye"></i>';
-$icon['eye_slash']  = '<i class="bi bi-eye-slash"></i>';
-
-$icon['chart_bar'] = '<i class="bi bi-bar-chart"></i>';
-
-$icon['database'] = '<i class="bi bi-hdd-network"></i>';
-$icon['dot_circle'] = '<i class="bi bi-circle"></i>';
-$icon['download'] = '<i class="bi bi-download"></i>';
-
-$icon['file'] = '<i class="bi bi-file-earmark"></i>';
-$icon['file_alt'] = '<i class="bi bi-file-earmark"></i>';
-$icon['filter'] = '<i class="bi bi-filter"></i>';
-$icon['folder_open'] = '<i class="bi bi-folder2-open"></i>';
-
-$icon['github'] = '<i class="bi bi-github"></i>';
-
-$icon['info_circle'] = '<i class="bi bi-info-circle"></i>';
-$icon['images'] = '<i class="bi bi-images"></i>';
-
-$icon['language'] = '<i class="bi bi-translate"></i>';
-$icon['level_up_alt'] = '<i class="bi bi-arrow-90deg-up"></i>';
-$icon['link'] = '<i class="bi bi-link"></i>';
-$icon['list'] = '<i class="bi bi-list"></i>';
-$icon['lock'] = '<i class="bi bi-lock"></i>';
-$icon['lock_open'] = '<i class="bi bi-unlock"></i>';
-$icon['long_arrow_alt_right'] = '<i class="bi bi-arrow-right"></i>';
-
-$icon['map_marker_alt'] = '<i class="bi bi-geo-alt"></i>';
-
-$icon['paper_clip'] = '<i class="bi bi-paperclip"></i>';
-$icon['paper_plane'] = '<i class="bi bi-send"></i>';
-$icon['paste'] = '<i class="bi bi-clipboard-plus"></i>';
-
-
-$icon['search'] = '<i class="bi bi-search"></i>';
-$icon['server'] = '<i class="bi bi-server"></i>';
-$icon['share'] = '<i class="bi bi-share"></i>';
-$icon['shopping_basket'] = '<i class="bi bi-basket"></i>';
-$icon['sign_out_alt'] = '<i class="bi bi-box-arrow-left"></i>';
-
-$icon['star'] = '<i class="bi-star-fill"></i>';
-$icon['star_outline'] = '<i class="bi bi-star"></i>';
-$icon['store'] = '<i class="bi bi-shop"></i>';
-
-$icon['table'] = '<i class="bi bi-table"></i>';
-$icon['tags'] = '<i class="bi bi-tags"></i>';
-$icon['tasks'] = '<i class="bi bi-list-task"></i>';
-$icon['thumbs_up'] = '<i class="bi bi-hand-thumbs-up"></i>';
-$icon['thumbs_down'] = '<i class="bi bi-hand-thumbs-down"></i>';
-
-$icon['times_circle'] = '<i class="bi bi-clock"></i>';
-$icon['tools'] = '<i class="bi bi-tools"></i>';
-$icon['toggle_on'] = '<i class="bi bi-toggle-on"></i>';
-$icon['toggle_off'] = '<i class="bi bi-toggle-off"></i>';
-
-$icon['users'] = '<i class="bi bi-people"></i>';
-$icon['user'] = '<i class="bi bi-person"></i>';
-$icon['user_check'] = '<i class="bi bi-person-check"></i>';
-$icon['user_friends'] = '<i class="bi bi-person-hearts"></i>';
-$icon['user_lock'] = '<i class="bi bi-person-x"></i>';
-$icon['user_minus']  = '<i class="bi bi-person-dash"></i>';
-$icon['user_plus'] = '<i class="bi bi-person-plus"></i>';
-$icon['user_slash'] = '<i class="bi bi-person-dash"></i>';
-
-$icon['wrench'] = '<i class="bi bi-wrench"></i>';
