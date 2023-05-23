db2408a062878aa8d4dea4482e1aace4f09b3aae
Patrick <pk@konstand.de>
2023-05-23 19:51:03 +0200

    New functions for page management
    The saving and updating process of pages, as well as the preview function has been rewritten. The goal is to be able to update pages without sending all data but still keeping version control intact. For example if only the title is updated.

---
+++ core/functions/functions.pages.php
@@ -0,0 +1,159 @@
+<?php
+
+/**
+ * write and update pages
+ * do snapshots from pages
+ */
+
+/**
+ * @param array $data $_POST data
+ * @return mixed
+ */
+function se_save_page($data) {
+
+    global $db_content, $custom_fields, $lang;
+
+    $sanitized_data = se_sanitize_page_inputs($data);
+
+    // get all cols from the installer
+    require '../install/contents/se_pages.php';
+
+    // add custom cols
+    foreach($custom_fields as $f) {
+        $cols[$f] = "";
+    }
+
+    // loop through sanitized data
+    // if key exists in $cols -> insert
+    foreach($sanitized_data as $k => $v) {
+        if(array_key_exists($k,$cols)) {
+            $insert[$k] = $v;
+        }
+    }
+
+    $cnt_changes = $db_content->insert("se_pages",$insert);
+    $new_page_id = $db_content->id();
+
+    if($cnt_changes->rowCount() > 0) {
+        record_log("$_SESSION[user_nick]","new Page <i>$page_title</i>","5");
+        generate_xml_sitemap();
+        show_toast($lang['msg_page_saved'],'success');
+    } else {
+        show_toast($lang['msg_page_saved_error'],'danger');
+    }
+
+    return $new_page_id;
+}
+
+/**
+ * @param array $data $_POST data
+ * @param integer $id page_id
+ * @return void
+ */
+function se_update_page($data,$id) {
+
+    global $db_content, $custom_fields, $lang;
+    $id = (int) $id;
+
+
+    $sanitized_data = se_sanitize_page_inputs($data);
+
+    // get all cols from the installer
+    require '../install/contents/se_pages.php';
+
+    // add custom cols
+    foreach($custom_fields as $f) {
+        $cols[$f] = "";
+    }
+
+    // loop through sanitized data
+    // if key exists in $cols -> update
+    foreach($sanitized_data as $k => $v) {
+        if(array_key_exists($k,$cols)) {
+            $updates[$k] = $v;
+        }
+    }
+
+    $cnt_changes = $db_content->update("se_pages", $updates, [
+        "page_id" => $id
+    ]);
+
+    if($cnt_changes->rowCount() > 0) {
+        record_log("$_SESSION[user_nick]","page update <b>$page_linkname</b> &raquo;$page_title&laquo;","5");
+        generate_xml_sitemap();
+        show_toast($lang['msg_page_updated'],'success');
+    } else {
+        show_toast($lang['msg_page_saved_error'],'danger');
+    }
+
+}
+
+/**
+ * @param array $data $_POST data
+ * @return void
+ */
+function se_save_preview_page($data) {
+    global $db_content, $custom_fields, $lang;
+
+    $sanitized_data = se_sanitize_page_inputs($data);
+    $page_id_original = $sanitized_data['editpage'];
+    // get all cols from the installer
+    require '../install/contents/se_pages.php';
+
+    // add custom cols
+    foreach($custom_fields as $f) {
+        $cols[$f] = "";
+    }
+
+    // loop through sanitized data
+    // if key exists in $cols -> insert
+    foreach($sanitized_data as $k => $v) {
+        if(array_key_exists($k,$cols)) {
+            $insert[$k] = $v;
+        }
+    }
+
+    $insert += [
+        "page_id_original" => $page_id_original,
+        "page_cache_type" => "preview"
+    ];
+
+    $db_content->insert("se_pages_cache",$insert);
+}
+
+
+
+/**
+ * Take a snapshot of a page
+ * get all data from se_pages by id
+ * @param $id
+ * @return void
+ */
+function se_snapshot_page($id) {
+
+    global $db_content, $custom_fields;
+    $id = (int) $id;
+
+    $get_data = $db_content->get("se_pages", "*", [
+        "page_id" => $id
+    ]);
+
+    foreach($get_data as $k => $v) {
+        $columns_cache[$k] = $v;
+    }
+
+    $columns_cache += [
+        "page_id_original" => "$id",
+        "page_cache_type" => "history"
+    ];
+
+    /* add the custom fields */
+    foreach($custom_fields as $f) {
+        $columns_cache[$f] = "${$f}";
+    }
+
+    /* reset id */
+    unset($columns_cache['page_id']);
+
+    $db_content->insert("se_pages_cache", $columns_cache);
+}
--- core/functions/functions.sanitizer.php
+++ core/functions/functions.sanitizer.php
@@ -109,6 +109,210 @@ function clean_visitors_input($text) {
 
 }
 
+/**
+ * sanitize inputs for pages
+ * we use this for new pages or update pages
+ *
+ * @param array $data $_POST Data
+ * @return array
+ */
+function se_sanitize_page_inputs($data) {
+
+    global $se_prefs;
+
+    foreach($data as $key => $val) {
+
+        // main content
+        if($key == 'page_content') {
+            $sanitized[$key] = $val;
+        }
+
+        // strings
+        if($key == 'page_title' || $key == 'page_linkname' || $key == 'page_custom_id'
+            || $key == 'page_classes' || $key == 'page_custom_classes'
+            || $key == 'page_meta_keywords' || $key == 'page_meta_author'
+            || $key == 'page_meta_description' || $key == 'page_modul'
+            || $key == 'page_language' || $key == 'page_status' || $key == 'page_type_of_use'
+            || $key == 'page_funnel_uri' || $key == 'page_target' || $key == 'page_template_stylesheet') {
+            $sanitized[$key] = se_return_clean_value($val);
+        }
+
+        // filenames
+        if($key == 'page_hash') {
+            $sanitized[$key] = clean_filename($val);
+        }
+
+        // urls
+        if($key == 'page_permalink' || $key == 'page_permalink_short' || $key == 'page_redirect') {
+            $sanitized[$key] = se_clean_permalink($val);
+        }
+
+        // integers
+        if($key == 'page_priority' || $key == 'page_categories_mode' || $key == 'page_comments' || $key == 'editpage'
+            || $key == 'page_redirect_code') {
+            $sanitized[$key] = (int) $val;
+        }
+
+        // thumbnails
+        if($key == 'picker1_images') {
+            if(count($data['picker1_images']) > 1) {
+                $page_thumbnail = implode("<->", $_POST['picker1_images']);
+            } else {
+                $pt = $_POST['picker1_images'];
+                $page_thumbnail = $pt[0];
+            }
+            $sanitized['page_thumbnail'] = se_return_clean_value($page_thumbnail);
+        }
+
+        // password
+        if($key == 'page_psw') {
+            if($val != '') {
+                $sanitized['page_psw'] = md5($val);
+            }
+        }
+        if($key == 'page_psw_reset') {
+            if($val == 'reset') {
+                $sanitized['page_psw'] = '';
+            }
+        }
+
+        // labels
+        if($key == 'set_page_labels') {
+            if(is_array($val)) {
+                sort($val);
+                $sanitized['page_labels'] = implode(",", $val);
+            }
+        }
+
+        // categories
+        if($key == 'set_page_categories') {
+            if(is_array($val)) {
+                sort($val);
+                $sanitized['page_categories'] = implode(",", $val);
+            }
+        }
+
+        // usergroups
+        if($key == 'set_usergroup') {
+            if(is_array($val)) {
+                sort($val);
+                $sanitized['page_usergroup'] = implode(",", $val);
+            }
+        }
+
+        // set_authorized_admins
+        if($key == 'set_authorized_admins') {
+            if(is_array($val)) {
+                sort($val);
+                $sanitized['page_authorized_users'] = implode(",", $val);
+            }
+        }
+
+        // page meta robots
+        if($key == 'page_meta_robots') {
+            if(is_array($val)) {
+                sort($val);
+                $sanitized['page_meta_robots'] = implode(",", $val);
+            }
+        }
+
+        // select template
+        if($key == 'select_template') {
+            $tpl_parts = explode("<|-|>",$val);
+            $sanitized['page_template'] = $tpl_parts[0];
+            $sanitized['page_template_layout'] = $tpl_parts[1];
+        }
+
+        // post categories
+        if($key == 'page_post_categories') {
+            $sanitized['page_posts_categories'] = implode(",", $val);
+        }
+
+        // post types
+        if($key == 'page_post_types') {
+            if(is_array($val)) {
+                $sanitized['page_posts_types'] = implode(",", $val);
+            }
+        }
+
+        // custom columns
+        if(str_starts_with($key,"custom_one_")) {
+            $sanitized[$key] = se_return_clean_value($val);
+        }
+        if(str_starts_with($key,"custom_text_")) {
+            $sanitized[$key] = se_return_clean_value($val);
+        }
+        if(str_starts_with($key,"custom_wysiwyg_")) {
+            $sanitized[$key] = $val;
+        }
+
+        // addon_values
+        if($key == 'addon_values') {
+            if(is_array($val)) {
+                foreach($val as $k => $v) {
+                    $addon_values[$k] = htmlentities(stripslashes($v), ENT_QUOTES);
+                }
+                $sanitized['page_addon_string'] = json_encode($addon_values,JSON_UNESCAPED_UNICODE);
+            }
+        }
+
+        // theme values
+        if($key == 'theme_values') {
+            if(is_array($val)) {
+                foreach($val as $k => $v) {
+                    $theme_values[$k] = htmlentities(stripslashes($v), ENT_QUOTES);
+                }
+                $sanitized['page_template_values'] = json_encode($theme_values,JSON_UNESCAPED_UNICODE);
+            }
+        }
 
 
-?>
+    }
+
+    $sanitized['page_version'] = (int) $data['page_version']+1;
+    $sanitized['page_version_date'] = time();
+    $sanitized['page_lastedit'] = time();
+    $sanitized['page_meta_date'] = time();
+    $sanitized['page_lastedit_from'] = $_SESSION['user_nick'];
+
+    // page sort
+    if($data['page_position'] == "portal") {
+        $sanitized['page_sort'] = "portal";
+    } else if($data['page_position'] == "mainpage") {
+        $sanitized['page_sort'] = (int) $data['page_order'];
+    } else if($data['page_position'] == "null") {
+        $sanitized['page_sort'] = "";
+    } else {
+        $page_order = (int) $data['page_order'];
+        if(strlen($page_order) < $se_prefs['prefs_pagesort_minlength']) {
+            $page_order = str_pad($page_order, $se_prefs['prefs_pagesort_minlength'], "0", STR_PAD_LEFT);
+        }
+        $sanitized['page_sort'] = $data['page_position'].'.'.$page_order;
+    }
+
+    // resets
+    if(!is_array($data['set_authorized_admins'])) {
+        $sanitized['page_authorized_users'] = "";
+    }
+    if(!is_array($data['set_usergroup'])) {
+        $sanitized['page_usergroup'] = "";
+    }
+    if(!is_array($data['set_page_categories'])) {
+        $sanitized['page_categories'] = "";
+    }
+    if(!is_array($data['set_page_labels'])) {
+        $sanitized['page_labels'] = "";
+    }
+    if(!is_array($data['page_post_types'])) {
+        $sanitized['page_posts_types'] = "";
+    }
+    if(!is_array($data['page_post_categories'])) {
+        $sanitized['page_posts_categories'] = "";
+    }
+    if(!is_array($data['theme_values'])) {
+        $sanitized['page_template_values'] = "";
+    }
+
+
+    return $sanitized;
+}
