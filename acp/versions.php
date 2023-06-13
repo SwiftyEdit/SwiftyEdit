85481662ff89575316a6ff653d0ee86d63750b94
Patrick <pk@konstand.de>
2023-06-13 09:52:33 +0200

    Update versions.php

--- acp/versions.php
+++ acp/versions.php
@@ -1,4 +1,4 @@
 <?php
-$se_version_date = '2023-06-12';
-$se_version_title = '1.1.0';
-$se_version_build = '374';
+$se_version_date = '2023-06-13';
+$se_version_title = '1.1.1';
+$se_version_build = '375';
