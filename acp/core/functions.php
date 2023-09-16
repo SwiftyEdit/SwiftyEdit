924c7c85285f7a56e08820748e04f9460f5c1b58
Patrick <pk@konstand.de>
2023-09-16 15:49:45 +0200

    fix - fatal error when there are no docs for the chossen language

--- acp/core/functions.php
+++ acp/core/functions.php
@@ -966,21 +966,21 @@ function se_unique_multi_array($array, $key) {
  *
  */
 
-function se_array_multisort(){
-	$args = func_get_args();
-  $data = array_shift($args);
-  foreach($args as $n => $field) {
-  	if(is_string($field)) {
-			$tmp = array();
-      foreach ($data as $key => $row){
-      	$tmp[$key] = $row[$field];
-        $args[$n] = $tmp;
-			}
+function se_array_multisort() {
+    $args = func_get_args();
+    $data = array_shift($args);
+    foreach ($args as $n => $field) {
+        if (is_string($field)) {
+            $tmp = array();
+            foreach ($data as $key => $row) {
+                $tmp[$key] = $row[$field];
+                $args[$n] = $tmp;
+            }
+        }
     }
-  }
-  $args[] = &$data;
-  call_user_func_array('array_multisort', $args);
-  return array_pop($args);
+    $args[] = &$data;
+    call_user_func_array('array_multisort', $args);
+    return array_pop($args);
 }
 
 
