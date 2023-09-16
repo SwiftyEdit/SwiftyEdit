252311e8d83f6fcd22be8417d503ea335fc46eaf
Patrick <pk@konstand.de>
2023-09-16 15:59:05 +0200

    Added Spanish

---
+++ core/lang/es/dict-frontend.php
@@ -0,0 +1,246 @@
+<?php
+
+
+/* Headlines */
+$lang['legend_login'] = "Iniciar sesión";
+$lang['legend_register'] = "Registrarse";
+$lang['legend_delete_account'] = "Eliminar cuenta";
+$lang['legend_required_fields'] = "Campos obligatorios";
+$lang['legend_optional_fields'] = "Datos opcionales";
+$lang['legend_address_fields'] = "Dirección/contacto";
+$lang['legend_access_data'] = "Datos de acceso";
+$lang['legend_avatar'] = "Imagen de usuario";
+$lang['legend_searchbox'] = "Página de búsqueda";
+$lang['legend_lastedit'] = "Última actualización";
+$lang['legend_mostclicked'] = "Más leído";
+$lang['legend_tags'] = "Palabras clave";
+$lang['legend_toc'] = "Contenido";
+
+$lang['headline_editprofile'] = "Editar perfil";
+$lang['headline_searchresults'] = "Buscar";
+
+$lang['title_404'] = "404 | Página no encontrada";
+$lang['msg_404'] = "Lo sentimos, la página no existe (o ya no existe)";
+
+$lang['menu'] = "Menú";
+
+/* Labels */
+$lang['label_login'] = "Login";
+$lang['label_psw'] = "Contraseña";
+$lang['label_psw_repeat'] = "Confirmar contraseña";
+$lang['label_psw_protected_page'] = "Página protegida con contraseña";
+$lang['label_username'] = "Nombre de usuario";
+$lang['label_billing_address'] = "Dirección de facturación y envío";
+$lang['label_company'] = "Empresa";
+$lang['label_lastname'] = "Nombre";
+$lang['label_firstname'] = "Nombre";
+$lang['label_mail'] = "Dirección de correo electrónico";
+$lang['label_mailrepeat'] = "Confirmar dirección de correo electrónico";
+$lang['label_street'] = "Calle";
+$lang['label_nr'] = "Nº";
+$lang['label_zip'] = "Código postal";
+$lang['label_town'] = "Ciudad";
+$lang['label_country'] = "País";
+$lang['label_please_select'] = "Por favor, seleccione...";
+$lang['label_tel'] = "Teléfono";
+$lang['label_fax'] = "Fax";
+$lang['label_about_you'] = "Sobre usted (público)";
+$lang['label_remember_me'] = "Permanecer conectado";
+
+$lang['label_entries'] = 'Entradas';
+$lang['label_entries_total'] = 'Total';
+$lang['label_search'] = 'Búsqueda';
+$lang['label_reset'] = 'Restablecer';
+
+$lang['label_product_variants'] = 'Variantes del artículo';
+$lang['label_nbr_of_product_variants'] = 'Este artículo está disponible en %s variantes';
+$lang['label_product_features'] = 'Características';
+$lang['label_product_description'] = 'Descripción';
+$lang['label_related_products'] = 'Artículos relacionados';
+$lang['label_products_accessories'] = 'Accesorios';
+
+$lang['label_prices_discount'] = 'Precios a escala';
+$lang['label_prices_snippet'] = 'Notas sobre los precios';
+$lang['price_tag_label_gross'] = 'IVA incl.';
+$lang['price_tag_label_net'] = 'más IVA';
+$lang['label_product_delivery_time'] = 'Plazo de entrega';
+
+
+/* Buttons, Links */
+$lang['button_login'] = "Iniciar sesión";
+$lang['button_profile'] = "Editar perfil";
+$lang['button_orders'] = "Pedidos";
+$lang['button_logout'] = "Cerrar sesión";
+$lang['button_save'] = "Guardar";
+$lang['button_update'] = "Actualizar";
+$lang['button_delete'] = "Borrar";
+$lang['button_send'] = "Enviar";
+$lang['button_search'] = "Buscar";
+$lang['button_send_register'] = "Enviar registro";
+$lang['button_back'] = "Volver";
+$lang['button_next'] = "continuar";
+$lang['button_top'] = "al principio";
+$lang['button_acp'] = "administración";
+$lang['button_acp_edit_page'] = "editar página";
+$lang['link_delete_avatar'] = "Eliminar imagen de usuario";
+$lang['link_register'] = "Regístrese ahora ...";
+$lang['btn_read_more'] = "Leer más";
+$lang['btn_download'] = "Descargar";
+$lang['btn_show_gallery'] = "Mostrar todas las {cnt_images} imágenes";
+$lang['btn_open_product'] = "Más información";
+$lang['btn_add_to_cart'] = "Añadir al carrito";
+$lang['btn_all'] = "Todos";
+$lang['btn_run_commands'] = "Ejecutar comandos";
+
+/* Messages */
+$lang['msg_missingfield'] = "Se deben rellenar todos los campos";
+$lang['msg_login_false'] = "No hay datos de usuario que coincidan";
+$lang['msg_login_true'] = "Bienvenido";
+$lang['msg_logout'] = "Se ha cerrado la sesión correctamente";
+$lang['msg_register'] = "¿Aún no te has registrado? Puedes registrarte aquí";
+$lang['msg_edit_psw'] = "Rellene estos campos sólo si desea cambiar la contraseña";
+$lang['msg_edit_mail'] = "Rellene estos campos sólo si desea cambiar su dirección de correo electrónico. Después de cambiar la dirección de correo electrónico, recibirás automáticamente un enlace de activación enviado a la nueva dirección.";
+$lang['msg_invalid_mail_format'] = "La dirección de correo electrónico contiene caracteres ilegales";
+$lang['msg_avatar'] = 'Muestra un gráfico junto a cada uno de tus mensajes.';
+$lang['msg_delete_account'] = "Aquí puedes eliminar tu cuenta. Esto eliminará irrevocablemente tus datos personales de la base de datos";
+$lang['msg_register_error'] = "Se han producido errores durante el registro.";
+$lang['msg_register_accept'] = "Debes aceptar la política de privacidad";
+$lang['msg_register_requiredfields'] = "Debe rellenar todos los campos obligatorios";
+$lang['msg_register_userchars'] = "Der angegebene Benutzername enthält zuviel, zuwenig oder unerlaubte Zeichen.";
+$lang['msg_register_existinguser'] = "Der angegebene Benutzername ist bereits vergeben oder nicht erlaubt.";
+$lang['msg_register_existingusermail'] = "Die angegebene E-Mail Adresse ist bereits vergeben.";
+$lang['msg_register_mailrepeat_error'] = "Die E-Mail Adressen stimmen nicht überein.";
+$lang['msg_register_pswrepeat_error'] = "Die Passwörter stimmen nicht überein.";
+$lang['msg_register_success'] = "Der Benutzeraccount wurde erstellt und ein Aktivierungslink an die angegebene E-Mail Adresse gesendet.";
+$lang['msg_register_admin_notification_subject'] = "Neuer Benutzer";
+$lang['msg_register_admin_notification_text'] = "Ein neuer Benutzeraccount wurde erstellt.";
+$lang['msg_confirm_delete_account'] = "Diesen Benutzeraccount wirklich löschen?";
+$lang['msg_delete_account_success'] = "Der Benutzeraccount wurde aus der Datenbank entfernt";
+$lang['msg_delete_account_error'] = "Ein Fehler ist aufgetreten, der Account wurde nicht gelöscht.";
+$lang['msg_register_intro'] = "Por favor, rellene el siguiente formulario y haga clic en <b>Submit Registration</b>. En breve recibirás un correo electrónico con un enlace de activación en el que deberás hacer clic para activar tu cuenta.";
+$lang['msg_register_intro_disabled'] = "No es posible registrarse en este momento.";
+$lang['msg_register_outro'] = "Confirme aquí que ha leído y acepta nuestras Condiciones de uso.";
+$lang['msg_update_profile'] = "Se han actualizado los datos del usuario";
+$lang['msg_update_profile_error'] = "No se han podido actualizar los datos del usuario";
+$lang['msg_upload_avatar_success'] = "Se ha actualizado la imagen del usuario";
+$lang['msg_upload_avatar_filesize'] = "La imagen de usuario es demasiado grande";
+$lang['msg_upload_avatar_filetype'] = "La imagen debe ser de tipo PNG";
+
+$lang['msg_no_products_found'] = "No se encontraron productos";
+$lang['msg_no_entries_found'] = "No se han encontrado artículos";
+
+$lang['msg_item_needs_upload'] = "Necesitamos un archivo para este artículo. Puedes subir el archivo una vez que el pedido esté completo";
+
+$lang['msg_search_undersized'] = "La palabra de búsqueda debe tener al menos 3 letras";
+$lang['msg_search_results'] = "La consulta de búsqueda devolvió %d resultados:";
+$lang['msg_search_no_results'] = "La consulta de búsqueda no ha devuelto ningún resultado";
+$lang['forgotten_psw'] = "¿Has olvidado tu contraseña?";
+$lang['forgotten_psw_mail_subject'] = "Tus datos de acceso";
+$lang['msg_forgotten_psw_step1'] = "Ya puede restablecer su contraseña. La clave de activación necesaria acaba de ser enviada a la dirección de correo electrónico que dejaste";
+$lang['msg_forgotten_psw_step2'] = "Su nueva contraseña ha sido enviada a su dirección de correo electrónico";
+$lang['legend_ask_for_psw'] = "Restablecer contraseña.";
+$lang['forgotten_psw_intro'] = "Introduzca la dirección de correo electrónico de su cuenta de usuario en el siguiente campo de entrada y haga clic en Enviar. Recibirás inmediatamente un correo electrónico con la información necesaria sobre cómo restablecer tu contraseña.";
+$lang['forgotten_psw_mail_info'] = "Hola, {USERNAME},<br />¿Quieres restablecer tu contraseña? Simplemente haz clic en el enlace a continuación para obtener una nueva contraseña.<br />Ignora este correo electrónico, si la solicitud restablecer su contraseña no proviene de usted.<br /><br />Restablecer contraseña:<br />{RESET_LINK}";
+$lang['forgotten_psw_mail_update'] = "<p>Hola, {USERNAME},<br />Tu contraseña ha sido restablecida.</p> <p>Tu nueva contraseña es:<br />{temp_psw}</p> " ;
+
+$lang['posts_category_filter'] = 'Sólo se mostrarán publicaciones de la categoría <strong>{categorie}</strong>.';
+$lang['post_is_draft'] = 'Esta publicación es un borrador. Sólo los administradores pueden ver esta entrada.';
+
+/* comment */
+$lang['label_name'] = "Nombre";
+$lang['label_mail'] = "Correo electrónico";
+$lang['label_mail_helptext'] = "Su dirección de correo electrónico no se publicará aquí";
+$lang['label_comment'] = "Comentario";
+$lang['label_comment_answer'] = "Responder a";
+$lang['btn_send_comment'] = 'Ingresar comentario';
+$lang['btn_send_answer'] = 'Respuesta';
+$lang['comment_msg_sucess'] = 'Gracias por tu comentario';
+$lang['comment_msg_fail'] = 'Se ha producido un error. Por favor, inténtelo de nuevo más tarde.';
+
+$lang['comment_form_title'] = 'Escribir un comentario';
+$lang['comment_msg_auth1'] = 'Debes iniciar sesión para publicar comentarios.';
+$lang['comment_msg_auth2'] = 'Debe proporcionar su nombre y dirección de correo electrónico.';
+$lang['comments_title'] = 'Comentarios ({cnt_comments})';
+
+
+/* guest list */
+$lang['label_guestlist'] = 'Lista de invitados';
+$lang['guestlist_description'] = 'Puedes agregarte a la lista de invitados.';
+$lang['guestlist_description_for_members'] = 'Sólo los usuarios registrados pueden agregarse a la lista de invitados.';
+$lang['btn_guestlist_sign'] = 'Participar';
+
+$lang['guestlist_label_nbr_total_available'] = 'Lugares disponibles:';
+$lang['guestlist_label_nbr_commitments'] = 'Compromisos:';
+
+/* shopping cart and checkout */
+$lang['label_shopping_cart'] = 'Carrito de compras';
+$lang['label_cnt_sc_items'] = 'Artículos en el carrito';
+$lang['label_price'] = 'Precio';
+$lang['label_product_amount'] = 'Cantidad';
+$lang['label_product_info'] = 'Producto';
+$lang['label_tax'] = 'Impuesto';
+$lang['label_net'] = 'Neto';
+$lang['label_gross'] = 'Bruto';
+$lang['price_single'] = 'Precio único';
+$lang['shipping_costs'] = 'Costos de envío';
+$lang['price_subtotal'] = 'Subtotal';
+$lang['price_total'] = 'Precio total';
+$lang['label_pm_bank_transfer'] = 'Transferencia';
+$lang['label_pm_invoice'] = 'Factura';
+$lang['label_pm_cash'] = 'Efectivo';
+$lang['label_pm_paypal'] = 'PayPal';
+$lang['label_paid'] = 'Pago';
+$lang['label_paid_method'] = 'Método de pago';
+$lang['label_invoice_address'] = 'Dirección de factura';
+$lang['label_cart_comment'] = 'Agregar comentario o nota al pedido';
+$lang['msg_accept_terms'] = 'Debe confirmar que ha leído y aceptado los avisos y términos legales.';
+$lang['msg_order_send'] = '<b>¡Gracias!</b> Su pedido ha sido enviado.';
+$lang['msg_request_send'] = '<b>¡Gracias!</b> Su solicitud ha sido enviada.';
+$lang['request_form_intro'] = 'Por favor, introduzca su nombre y dirección de correo electrónico para que podamos procesar su solicitud.';
+$lang['msg_missing_mandatory_informations'] = 'Necesitamos su dirección completa para completar el pedido';
+$lang['btn_send_order'] = 'Comprar';
+$lang['btn_send_order_request'] = 'Enviar solicitud';
+$lang['order_status'] = 'Estado';
+$lang['label_orders'] = 'Pedidos';
+$lang['label_order_nbr'] = 'Número de pedido';
+$lang['status_paid_open'] = 'Pago pendiente';
+$lang['status_paid_paid'] = 'Pagado';
+$lang['label_shipping'] = 'Envío';
+$lang['status_shipping_open'] = 'Preparándose para el envío';
+$lang['status_shipping_done'] = 'Enviado';
+$lang['mail_subj_status_paid_done'] = 'Hemos recibido su pago';
+$lang['mail_subj_status_shipping_done'] = 'Su pedido ha sido enviado';
+
+$lang['label_sort_price_asc'] = 'Precio ascendente';
+$lang['label_sort_price_desc'] = 'Precio descendente';
+$lang['label_sort_topseller'] = 'Más vendido';
+$lang['label_sort_name'] = 'Nombre';
+
+/* Dates */
+
+$lang['label_date'] = "Fecha";
+
+$lang['m01'] = "Enero";
+$lang['m02'] = "Febrero";
+$lang['m03'] = "Marzo";
+$lang['m04'] = "Abril";
+$lang['m05'] = "Mayo";
+$lang['m06'] = "Junio";
+$lang['m07'] = "Julio";
+$lang['m08'] = "agosto";
+$lang['m09'] = "Septiembre";
+$lang['m10'] = "Octubre";
+$lang['m11'] = "Noviembre";
+$lang['m12'] = "Diciembre";
+
+
+$lang['d01'] = "Lunes";
+$lang['d02'] = "Martes";
+$lang['d03'] = "Miércoles";
+$lang['d04'] = "Jueves";
+$lang['d05'] = "Viernes";
+$lang['d06'] = "Sábado";
+$lang['d07'] = "Domingo";
+
+$lang['date_today'] = "Hoy";
+$lang['date_yesterday'] = "Ayer";
---
+++ core/lang/es/index.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * language file | spanish
+ */
+
+$lang_sign = "es";
+$lang_desc = "Espaniol";
