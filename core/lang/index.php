f250ea9fdbdf874b8feac36aebce7e6bc7fbd068
Patrick <pk@konstand.de>
2024-03-22 11:45:32 +0100

    use the /en/ language files as fallback

--- core/lang/index.php
+++ core/lang/index.php
@@ -31,17 +31,17 @@ if(SE_SECTION == 'frontend') {
 
 } elseif(SE_SECTION == 'backend') {
 
-    $json_backend = file_get_contents(SE_ROOT.'core/lang/'.$languagePack.'/backend.json');
-    $data_backend = json_decode($json_backend,true);
+    $default_lang_file_backend = SE_ROOT.'core/lang/en/backend.json';
+    $default_lang_file_dict = SE_ROOT.'core/lang/en/dictionary.json';
 
-    $json_frontend = file_get_contents(SE_ROOT.'core/lang/'.$languagePack.'/frontend.json');
-    $data_frontend = json_decode($json_frontend,true);
+    $default_backend = l10n_to_array($default_lang_file_backend);
+    $default_dict = l10n_to_array($default_lang_file_dict);
 
+    $data_backend = l10n_to_array(SE_ROOT.'core/lang/'.$languagePack.'/backend.json');
+    $data_frontend = l10n_to_array(SE_ROOT.'core/lang/'.$languagePack.'/frontend.json');
+    $data_dict = l10n_to_array(SE_ROOT.'core/lang/'.$languagePack.'/dictionary.json');
 
-    $json_dict = file_get_contents(SE_ROOT.'core/lang/'.$languagePack.'/dictionary.json');
-    $data_dict = json_decode($json_dict,true);
-
-    $lang_data = array_merge($data_backend,$data_frontend,$data_dict);
+    $lang_data = array_merge($default_backend,$default_dict,$data_frontend,$data_backend,$data_dict);
 
     foreach($lang_data as $key => $value) {
         $lang[str_replace('.','_',$key)] = $value;
@@ -55,4 +55,17 @@ if(SE_SECTION == 'frontend') {
 
 if(is_file($extend_lf)) {
 	include $extend_lf;
+}
+
+/**
+ * @param $file
+ * @return mixed
+ */
+function l10n_to_array($file) {
+    $data = array();
+    if(is_file($file)) {
+        $json = file_get_contents($file);
+        $data = json_decode($json, true);
+    }
+    return $data;
 }
