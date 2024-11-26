bb6e16581b8a3e18e8f6b9b3a6d34e8dfeab2297
Patrick <pk@konstand.de>
2024-11-26 08:45:44 +0100

    improved snippet listing

--- acp/core/snippets/data-reader.php
+++ acp/core/snippets/data-reader.php
@@ -4,7 +4,7 @@ $writer_uri = '/admin/snippets/edit/';
 $duplicate_uri = '/admin/snippets/duplicate/';
 
 // show list of keywords (search)
-if($_REQUEST['action'] == 'list_keywords') {
+if($_REQUEST['action'] == 'list_active_searches') {
     if(isset($_SESSION['snippets_text_filter']) AND $_SESSION['snippets_text_filter'] != "") {
         unset($all_filter);
         $all_filter = explode(" ", $_SESSION['snippets_text_filter']);
@@ -34,20 +34,48 @@ if($_REQUEST['action'] == 'list_snippets') {
     $nbr_show_items = 10;
 
     $match_str = $_SESSION['snippets_text_filter'] ?? '';
+    $keyword_str = $_SESSION['snippets_keyword_filter'] ?? '';
     $order_key = $_SESSION['sorting_snippets'] ?? $order_by;
     $order_direction = $_SESSION['sorting_snippet_direction'] ?? $order_direction;
 
-    $db_where = [
-        "AND" => [
-            "OR" => [
-            "snippet_title[~]" => "%$match_str%",
-            "snippet_name[~]" => "%$match_str%",
-            "snippet_content[~]" => "%$match_str%",
-            "snippet_keywords[~]" => "%$match_str%"
-                ],
-                "snippet_id[>]" => 0,
+    $filter_base = [
+      "AND" => [
+          "snippet_id[>]" => 0
+      ]
+    ];
 
-        ]];
+    $filter_by_str = array();
+    if($match_str != '') {
+        $this_filter = explode(" ",$match_str);
+        foreach($this_filter as $f) {
+            if($f == "") { continue; }
+            $filter_by_str = [
+                "OR" => [
+                    "snippet_title[~]" => "%$f%",
+                    "snippet_name[~]" => "%$f%",
+                    "snippet_content[~]" => "%$f%",
+                    "snippet_keywords[~]" => "%$f%"
+                ]
+            ];
+        }
+
+    }
+
+    $filter_by_keyword = array();
+    if($keyword_str != '') {
+        $this_filter = explode(" ",$keyword_str);
+        foreach($this_filter as $f) {
+            if($f == "") { continue; }
+            $filter_by_keyword = [
+                    "snippet_keywords[~]" => "%$f%"
+            ];
+        }
+
+    }
+
+    $db_where = [
+        "AND" => $filter_base+$filter_by_str+$filter_by_keyword
+    ];
 
     $db_order = [
         "ORDER" => [
@@ -114,5 +142,19 @@ if($_REQUEST['action'] == 'list_snippets') {
 
     echo '</table>';
 
+    echo '</div>';
+}
+
+if($_GET['action'] == 'list_keywords') {
+    $get_keywords = se_get_snippet_keywords();
+    echo '<div class="scroll-container">';
+    foreach($get_keywords as $k => $v) {
+        $k = trim($k);
+        if(str_contains($_SESSION['snippets_keyword_filter'],$k)) {
+            echo '<button name="remove_keyword" value="'.$k.'" hx-post="/admin/snippets/write/" hx-swap="none" hx-include="[name=\'csrf_token\']" class="btn btn-default active btn-xs mb-1">'.$k.' <span class="badge bg-secondary">'.$v.'</span></button> ';
+        } else {
+            echo '<button name="add_keyword" value="'.$k.'" hx-post="/admin/snippets/write/" hx-swap="none" hx-include="[name=\'csrf_token\']" class="btn btn-default btn-xs mb-1">'.$k.' <span class="badge bg-secondary">'.$v.'</span></button> ';
+        }
+    }
     echo '</div>';
 }
--- acp/core/snippets/snippets-list.php
+++ acp/core/snippets/snippets-list.php
@@ -19,8 +19,9 @@ echo '</div>';
 echo '</div>';
 echo '<div class="col-md-3">';
 
-echo '<div class="card p-3">';
-
+echo '<div class="card mb-2">';
+echo '<div class="card-header">'.$lang['filter'].'</div>';
+echo '<div class="card-body">';
 echo '<form hx-post="'.$writer_uri.'" hx-swap="none" hx-on--after-request="this.reset()" method="POST" class="mt-1">';
 echo '<div class="input-group">';
 echo '<span class="input-group-text">'.$icon['search'].'</span>';
@@ -29,10 +30,18 @@ echo $hidden_csrf_token;
 echo '</div>';
 echo '</form>';
 
-echo '<div id="keyList" hx-post="'.$reader_uri.'?action=list_keywords" hx-trigger="load, update_snippet_list from:body, updated_global_filter from:body" hx-include="[name=\'csrf_token\']"></div>';
-
+echo '<div class="pt-1" hx-get="'.$reader_uri.'?action=list_active_searches" hx-trigger="load, changed, update_snippet_list from:body, updated_global_filter from:body"></div>';
 
 echo '</div>';
+echo '</div>';
+
+echo '<div class="card">';
+echo '<div class="card-header">'.$lang['label_keywords'].'</div>';
+echo '<div class="card-body">';
+echo '<div id="keyList" hx-post="'.$reader_uri.'?action=list_keywords" hx-trigger="load, update_snippet_list from:body, updated_global_filter from:body" hx-include="[name=\'csrf_token\']"></div>';
+echo '</div>';
+echo '</div>';
+
 
 echo '</div>';
 echo '</div>';
