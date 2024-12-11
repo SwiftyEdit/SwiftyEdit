34c10a3a114dbede65e1e0aff56655834c34cd58
Patrick <pk@konstand.de>
2024-12-11 10:23:59 +0100

    Update data-reader.php
    fixed pagination

--- acp/core/uploads/data-reader.php
+++ acp/core/uploads/data-reader.php
@@ -12,7 +12,8 @@ if($_REQUEST['action'] == 'list') {
     $order_direction = 'DESC';
     $media_file = '../images';
     $limit_start = $_SESSION['pagination_page'] ?? 0;
-    $nbr_show_items = 10;
+    $nbr_show_items = 50;
+    $nbr_show_pages = 10;
 
     if($limit_start > 0) {
         $limit_start = ($limit_start*$nbr_show_items);
@@ -58,13 +59,53 @@ if($_REQUEST['action'] == 'list') {
 
     echo '<nav aria-label="Pagination">';
     echo '<ul class="pagination">';
+
+    // jump to the first page
+    echo '<li class="page-item">';
+    echo '<button class="page-link ms-1" hx-post="/admin/uploads/write/" hx-include="[name=\'csrf_token\']" name="pagination" value="0" hx-swap="none"><i class="bi bi-arrow-bar-left"></i></button>';
+    echo '</li>';
+
+    // jump to the previous page
+    $previous_page = $_SESSION['pagination_page']-1;
+    if($previous_page < 0) {
+        $previous_page = 0;
+    }
+    echo '<li class="page-item">';
+    echo '<button class="page-link" hx-post="/admin/uploads/write/" hx-include="[name=\'csrf_token\']" name="pagination" value="'.$previous_page.'" hx-swap="none"><i class="bi bi-arrow-left-short"></i></button>';
+    echo '</li>';
+
     for($i=0;$i<$nbr_pages;$i++) {
+
+        $show_number = $i+1;
+        $show_number_start = $_SESSION['pagination_page']-($nbr_show_pages/2);
+        $show_number_end = $_SESSION['pagination_page']+($nbr_show_pages/2);
+        // skip pages which doesn't match the range from $nbr_show_pages
+        if(($show_number < $show_number_start) || ($show_number > $show_number_end)) {
+            continue;
+        }
+
         $active = '';
         if($i == $_SESSION['pagination_page']) {
             $active = 'active';
         }
-        echo '<li class="page-item"><button class="page-link '.$active.'" hx-post="/admin/uploads/write/" hx-include="[name=\'csrf_token\']" name="pagination" value="'.$i.'" hx-swap="none">'.($i+1).'</button></li>';
+        echo '<li class="page-item">';
+        echo '<button class="page-link '.$active.'" hx-post="/admin/uploads/write/" hx-include="[name=\'csrf_token\']" name="pagination" value="'.$i.'" hx-swap="none">'.($i+1).'</button>';
+        echo '</li>';
     }
+
+    // jump to the next page
+    $next_page = $_SESSION['pagination_page'] + 1;
+    if($next_page > ($nbr_pages-1)) {
+        $next_page = $nbr_pages-1;
+    }
+    echo '<li class="page-item">';
+    echo '<button class="page-link" hx-post="/admin/uploads/write/" hx-include="[name=\'csrf_token\']" name="pagination" value="'.$next_page.'" hx-swap="none"><i class="bi bi-arrow-right-short"></i></button>';
+    echo '</li>';
+    // jump to the last page
+    echo '<li class="page-item">';
+    echo '<button class="page-link" hx-post="/admin/uploads/write/" hx-include="[name=\'csrf_token\']" name="pagination" value="'.($nbr_pages-1).'" hx-swap="none"><i class="bi bi-arrow-bar-right"></i></button>';
+    echo '</li>';
+
     echo '</ul>';
     echo '</nav>';
 
