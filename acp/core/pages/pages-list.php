1c0c03c62fa68b090b54a3956051b1945c7ac985
Patrick <pk@konstand.de>
2025-01-24 16:10:50 +0100

    minor improvements. in pages list

--- acp/core/pages/pages-list.php
+++ acp/core/pages/pages-list.php
@@ -28,31 +28,23 @@ echo '<div class="row">';
 echo '<div class="col-md-9">';
 
 if($_SESSION['show_page_type'] == 'sorted') {
-
-    echo '<div class="card">';
-    echo '<div class="card-header">' . $lang['nav_btn_structured_pages'] . '</div>';
-    echo '<div class="card-body">';
-    echo '<div class="scroll-box">';
-    echo '<div id="getPagesSorted" class="" hx-get="' . $reader_uri . '?action=list_pages_sorted" hx-trigger="load, changed, update_pages_list from:body, updated_global_filter from:body">';
-    echo '<div class="d-flex align-items-center htmx-indicator"><div class="spinner-border spinner-border-sm me-2" role="status"></div><span class="sr-only">Loading...</span></div>';
-    echo '</div>';
-    echo '</div>';
-    echo '</div>';
-    echo '</div>';
+    $card_title = $lang['nav_btn_structured_pages'];
+    $hx_uri = $reader_uri.'?action=list_pages_sorted';
+} else {
+    $card_title = $lang['nav_btn_single_pages'];
+    $hx_uri = $reader_uri.'?action=list_pages_single';
 }
 
-if($_SESSION['show_page_type'] == 'single') {
-    echo '<div class="card">';
-    echo '<div class="card-header">' . $lang['nav_btn_single_pages'] . '</div>';
-    echo '<div class="card-body">';
-    echo '<div class="scroll-box">';
-    echo '<div id="getPagesSingle" class="" hx-get="' . $reader_uri . '?action=list_pages_single" hx-trigger="load, changed, update_pages_list from:body, updated_global_filter from:body">';
-    echo '<div class="d-flex align-items-center htmx-indicator"><div class="spinner-border spinner-border-sm me-2" role="status"></div><span class="sr-only">Loading...</span></div>';
-    echo '</div>';
-    echo '</div>';
-    echo '</div>';
-    echo '</div>';
-}
+echo '<div class="card">';
+echo '<div class="card-header">' . $card_title . '</div>';
+echo '<div class="card-body">';
+echo '<div class="scroll-box">';
+echo '<div id="getPages" class="" hx-get="' . $hx_uri . '" hx-trigger="load, changed, update_pages_list from:body, updated_global_filter from:body">';
+echo '<div class="d-flex align-items-center htmx-indicator"><div class="spinner-border spinner-border-sm me-2" role="status"></div><span class="sr-only">Loading...</span></div>';
+echo '</div>';
+echo '</div>';
+echo '</div>';
+echo '</div>';
 
 echo '</div>';
 echo '<div class="col-md-3">';
@@ -67,6 +59,39 @@ echo '<a href="/admin/pages/?set_type=sorted" class="btn btn-default '.($_SESSIO
 echo '<a href="/admin/pages/?set_type=single" class="btn btn-default '.($_SESSION['show_page_type']  == "single" ? 'active' :'').'">'.$lang['nav_btn_single_pages'].'</a>';
 echo '</div>';
 
+if($_SESSION['show_page_type'] == 'single') {
+
+    $sorting_options = [
+        $lang['label_data_last_edit'] => 'lastedit',
+        $lang['label_link_name'] => 'linkname',
+        $lang['label_priority'] => 'priority'
+    ];
+
+    $input_select_sorting = [
+        "input_name" => "sorting_single_pages",
+        "input_value" => $_SESSION['sorting_single_pages'],
+        "label" => $lang['sorting'],
+        "options" => $sorting_options,
+        "type" => "select"
+    ];
+
+    echo '<form hx-post="' . $writer_uri . '" hx-swap="none" method="POST" class="mt-1 mb-0">';
+    echo '<div class="row mb-1">';
+    echo '<div class="col-md-8">';
+    echo se_print_form_input($input_select_sorting);
+    echo '</div>';
+    echo '<div class="col-md-4">';
+    echo '<label class="form-label">&nbsp;</label>';
+    echo '<div class="btn-group d-flex">';
+    echo '<button name="sorting_single_pages_asc" value="asc" title="' . $lang['btn_sort_asc'] . '" class="btn btn-sm btn-default w-100">' . $icon['arrow_up'] . '</button> ';
+    echo '<button name="sorting_single_pages_desc" value="desc" title="' . $lang['btn_sort_desc'] . '" class="btn btn-sm btn-default w-100">' . $icon['arrow_down'] . '</button>';
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+    echo $hidden_csrf_token;
+    echo '</form>';
+
+}
 
 echo '<form hx-post="'.$writer_uri.'" hx-swap="none" method="POST" hx-on::after-request="this.reset()" class="mt-1">';
 echo '<div class="input-group">';
@@ -80,38 +105,8 @@ echo '<div class="pt-1" hx-get="'.$reader_uri.'?action=list_active_searches" hx-
 
 echo '</div>';
 
-$sorting_options = [
-    $lang['label_data_last_edit'] => 'lastedit',
-    $lang['label_link_name'] => 'linkname',
-    $lang['label_priority'] => 'priority'
-];
 
-$input_select_page_status = [
-    "input_name" => "sorting_single_pages",
-    "input_value" => $_SESSION['sorting_single_pages'],
-    "label" => $lang['sorting'],
-    "options" => $sorting_options,
-    "type" => "select"
-];
-
-echo '<form hx-post="'.$writer_uri.'" hx-swap="none" method="POST" class="mt-1">';
-echo '<div class="row mb-3">';
-echo '<div class="col-md-8">';
-echo se_print_form_input($input_select_page_status);
-echo '</div>';
-echo '<div class="col-md-4">';
-echo '<label class="form-label">&nbsp;</label>';
-echo '<div class="btn-group d-flex">';
-echo '<button name="sorting_single_pages_asc" value="asc" title="'.$lang['btn_sort_asc'].'" class="btn btn-sm btn-default w-100">'.$icon['arrow_up'].'</button> ';
-echo '<button name="sorting_single_pages_desc" value="desc" title="'.$lang['btn_sort_desc'].'" class="btn btn-sm btn-default w-100">'.$icon['arrow_down'].'</button>';
-echo '</div>';
-echo '</div>';
-echo '</div>';
-echo $hidden_csrf_token;
-echo '</form>';
-
-
-echo '<div class="accordion accordion-flush" id="accordionExample">';
+echo '<div class="accordion accordion-flush mt-3" id="accordionExample">';
 echo '<div class="accordion-item">';
 echo '<h6 class="accordion-header"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
         '.$lang['label_keywords'].'
--- acp/templates/list-pages-item.tpl
+++ acp/templates/list-pages-item.tpl
@@ -7,16 +7,15 @@
 			<div class="col-12 col-xxl-8 flex-grow-1">
 				
 				<div class="row">
-					<div class="col-sm-2 col-lg-3 d-none d-lg-block">
-						<img src="{item-tmb-src}" class="img-fluid">
+					<div class="col-sm-2 d-none d-lg-block">
+						<div class="w-100 h-100 rounded" style="background-image: url({item-tmb-src}); background-size: contain; background-repeat: no-repeat;" ></div>
 					</div>
-					<div class="col-sm-10 col-lg-9">
+					<div class="col-sm-10">
 						<p class="mb-1"><a class="btn btn-default btn-sm" href="{frontend-link}" title="{frontend-link}"><i class="bi bi-box-arrow-in-up-right"></i> {item-linkname}</a></p>
 						<h5 class="mb-0">{item-title}</h5>
 						<p>{item-description}</p>
-						<p class="small border-top py-2">
+						<p class="small border-top py-1">
 							<i class="bi bi-link-45deg"></i> {item-permalink} <span class="text-primary">{item-redirect}</span><br>
-							<i class="bi bi-clock"></i> {item-lastedit}</p>
 						</p>
 					</div>
 
@@ -35,6 +34,7 @@
 					<p class="my-1"><i class="bi-palette"></i> {item-template}</p>
 					<p class="my-1"><i class="bi bi-tag"></i> {page_labels}</p>
 					<p>{item-mod}</p>
+					<p class="my-1"><i class="bi bi-clock"></i> {item-lastedit}</p>
 				</div>
 				
 
