6778aeab9539dcc8a4289bb6c7d2ff9929750aec
Patrick <pk@konstand.de>
2025-02-25 16:53:01 +0100

    update galleries (blog) in frontend

--- public/assets/themes/default/src/js/frontend.js
+++ public/assets/themes/default/src/js/frontend.js
@@ -7,10 +7,21 @@ window.jQuery = $; window.$ = $;
 
 import * as bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js';
 import './components/theme_switch.js';
-const { vote } = require('./components/vote');
-window.vote = vote;
+
 const { sign_guestlist } = require('./components/guestlist');
 window.sign_guestlist = sign_guestlist;
 
+import GLightbox from 'glightbox';
+window.glightbox = GLightbox;
+
 import htmx from "htmx.org/dist/htmx.esm";
 window.htmx = htmx;
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    const lightbox = GLightbox({
+        selector: '.lightbox',
+        touchNavigation: true,
+        loop: true,
+        autoplayVideos: true
+    });
+});
