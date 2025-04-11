08cb33541855699c8738b255aa74a4f28abc9240
Patrick <pk@konstand.de>
2025-04-11 08:38:37 +0200

    add the default frontend translations
    If a part of the frontend has not yet been translated, the English text is displayed

--- languages/index.php
+++ languages/index.php
@@ -43,16 +43,18 @@ if(SE_SECTION == 'frontend') {
 } elseif(SE_SECTION == 'backend') {
 
     $default_lang_file_backend = SE_ROOT.'languages/en/backend.json';
+    $default_lang_file_frontend = SE_ROOT.'languages/en/frontend.json';
     $default_lang_file_dict = SE_ROOT.'languages/en/dictionary.json';
 
     $default_backend = l10n_to_array($default_lang_file_backend);
+    $default_frontend = l10n_to_array($default_lang_file_frontend);
     $default_dict = l10n_to_array($default_lang_file_dict);
 
     $data_backend = l10n_to_array(SE_ROOT.'/languages/'.$languagePack.'/backend.json');
     $data_frontend = l10n_to_array(SE_ROOT.'/languages/'.$languagePack.'/frontend.json');
     $data_dict = l10n_to_array(SE_ROOT.'/languages/'.$languagePack.'/dictionary.json');
 
-    $lang_data = array_merge($default_backend,$default_dict,$data_frontend,$data_backend,$data_dict);
+    $lang_data = array_merge($default_backend,$default_frontend,$default_dict,$data_frontend,$data_backend,$data_dict);
 
     foreach($lang_data as $key => $value) {
         $lang[str_replace('.','_',$key)] = $value;
