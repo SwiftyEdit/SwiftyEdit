1c430ad635208a272c7e33596da26ad1dd855fab
Patrick <support@swiftyedit.com>
2025-09-08 13:09:54 +0200

    Refactor ACP router logic: inline `switch.php` into `index.php`

--- acp/index.php
+++ acp/index.php
@@ -110,7 +110,21 @@ if(!isset($query)) {
 }
 $se_path = explode("/", $query);
 
-require '../acp/core/switch.php';
+$se_sections = [
+    "pages","snippets","shortcodes",
+    "addons","users","categories",
+    "settings","shop","events",
+    "blog","inbox","uploads", "dashboard",
+    "update"
+];
+
+$se_section = 'dashboard';
+$maininc = "dashboard/router.php";
+
+if(in_array($se_path[0], $se_sections)) {
+    $se_section = $se_path[0];
+    $maininc = $se_section."/router.php";
+}
 
 
 $all_mods = se_get_all_addons();
