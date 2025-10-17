1c63e35cc9d4b162ef3dcf225fb918e96e430822
Patrick <support@swiftyedit.com>
2025-10-17 12:27:34 +0200

    Update product price calculation logic and bump version metadata:
    - Refactor `products.php` for improved readability and consistency in tax and volume discount handling.
    - Adjust formatting and logic in volume discount and price tag generation.
    - Update version metadata to `2025-10-17` and build number to `25-0127`.

--- app/xhr/products.php
+++ app/xhr/products.php
@@ -1,15 +1,21 @@
 <?php
 
-if(isset($_GET['calc']) && is_numeric($_GET['product_id'])) {
+/**
+ * @var array $se_settings
+ * @var array $lang
+ */
 
-    // get volume discount for this products
-    $get_product_id = (int) $_GET['product_id'];
-    $get_amount = (int) $_GET['amount'];
+if (isset($_GET['calc']) && is_numeric($_GET['product_id'])) {
+
+    // recalculate product price
+
+    $get_product_id = (int)$_GET['product_id'];
+    $get_amount = (int)$_GET['amount'];
 
     $product_data = se_get_product_data($get_product_id);
 
     // get price from price groups or from products data
-    if($product_data['product_price_group'] != '' AND $product_data['product_price_group'] != 'null') {
+    if ($product_data['product_price_group'] != '' and $product_data['product_price_group'] != 'null') {
         $price_data = se_get_price_group_data($product_data['product_price_group']);
         $product_tax = $price_data['tax'];
         $product_price_net = $price_data['price_net'];
@@ -19,32 +25,28 @@ if(isset($_GET['calc']) && is_numeric($_GET['product_id'])) {
         $product_price_net = $product_data['product_price_net'];
         $product_volume_discounts_json = $product_data['product_price_volume_discount'];
     }
-    if($product_tax == '1') {
+    if ($product_tax == '1') {
         $tax = $se_settings['posts_products_default_tax'];
-    } else if($product_tax == '2') {
+    } else if ($product_tax == '2') {
         $tax = $se_settings['posts_products_tax_alt1'];
     } else {
         $tax = $se_settings['posts_products_tax_alt2'];
     }
 
-    $post_prices = se_posts_calc_price($product_price_net,$tax);
+    $post_prices = se_posts_calc_price($product_price_net, $tax);
     $post_price_net = $post_prices['net'];
     $post_price_gross = $post_prices['gross'];
 
-    if($get_amount > 1) {
 
-        if ($product_volume_discounts_json != '') {
-            $product_volume_discounts = json_decode($product_volume_discounts_json, true);
+    if ($product_volume_discounts_json != '') {
+        $product_volume_discounts = json_decode($product_volume_discounts_json, true);
 
-            foreach ($product_volume_discounts as $k => $v) {
-                if ($get_amount >= $v['amount']) {
-                    $vd_price = se_posts_calc_price($v['price'], $tax);
-                }
+        foreach ($product_volume_discounts as $k => $v) {
+            if ($get_amount >= $v['amount']) {
+                $vd_price = se_posts_calc_price($v['price'], $tax);
+                $post_price_net = $vd_price['net'];
+                $post_price_gross = $vd_price['gross'];
             }
-
-            $post_price_net = $vd_price['net'];
-            $post_price_gross = $vd_price['gross'];
-
         }
     }
 
@@ -52,10 +54,10 @@ if(isset($_GET['calc']) && is_numeric($_GET['product_id'])) {
         // gross prices
         $product_price_tag = $post_price_gross;
         $product_tax_label = $lang['price_tag_label_gross'];
-    } else if($se_settings['prefs_posts_price_mode'] == 2) {
+    } else if ($se_settings['prefs_posts_price_mode'] == 2) {
         // gross and net prices
-        $product_price_tag = $post_price_net. '/'. $post_price_gross;
-        $product_tax_label = $lang['label_net']. ' / '. $lang['label_net'];
+        $product_price_tag = $post_price_net . '/' . $post_price_gross;
+        $product_tax_label = $lang['label_net'] . ' / ' . $lang['label_net'];
     } else {
         // net only (b2b mode)
         $product_price_tag = $post_price_net;
